186bbb1a30ce2ff150f1e78b1aab959b3b8d1232
grekuu <gracjanprusik@gmail.com>
2023-06-19 21:05:33 +0200

    feat(BeerDetail, BeerList): spinner

--- src/components/BeerDetail/BeerDetail.tsx
+++ src/components/BeerDetail/BeerDetail.tsx
@@ -3,6 +3,7 @@ import './beerDetail.scss'
 import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import { fetchAsyncSingleBeer, removeSelectedBeer, selectSingleBeer, setShowFooter } from '../../redux/beerSlice'
+import Spinner from 'react-bootstrap/Spinner'
 
 const BeerDetail = () => {
     const { beerId } = useParams()
@@ -20,46 +21,54 @@ const BeerDetail = () => {
 
     return (
         <div>
-            {beerDetail.map((selectedBeer) => (
-                <div className="beer-detail-container" key={selectedBeer.id}>
-                    <div className="beer-info">
-                        <img src={selectedBeer.image_url} alt={selectedBeer.name} />
-                        <div className="name">{selectedBeer.name}</div>
-                        <div className="tagline">{selectedBeer.tagline}</div>
-                        <div className="description">{selectedBeer.description}</div>
-                        <div>
-                            <b>ABV:</b> {selectedBeer.abv}
-                        </div>
-                        <div>
-                            <b>IBU:</b> {selectedBeer.ibu}
-                        </div>
-                        <div>
-                            <h4>Ingredients:</h4>
+            {beerDetail.length > 0 ? (
+                beerDetail.map((selectedBeer) => (
+                    <div className="beer-detail-container" key={selectedBeer.id}>
+                        <div className="beer-info">
+                            <img src={selectedBeer.image_url} alt={selectedBeer.name} />
+                            <div className="name">{selectedBeer.name}</div>
+                            <div className="tagline">{selectedBeer.tagline}</div>
+                            <div className="description">{selectedBeer.description}</div>
                             <div>
-                                <b>Malt:</b>
-                                <ul>
-                                    {selectedBeer.ingredients.malt.map((malt, id) => (
-                                        <li key={id}>
-                                            {malt.name} - {malt.amount.value} {malt.amount.unit}
-                                        </li>
-                                    ))}
-                                </ul>
+                                <b>ABV:</b> {selectedBeer.abv}
                             </div>
                             <div>
-                                <b>Hops:</b>
-                                <ul>
-                                    {selectedBeer.ingredients.hops.map((hop, id) => (
-                                        <li key={id}>
-                                            {hop.name} - {hop.amount.value} {hop.amount.unit} - {hop.add} -{' '}
-                                            {hop.attribute}
-                                        </li>
-                                    ))}
-                                </ul>
+                                <b>IBU:</b> {selectedBeer.ibu}
+                            </div>
+                            <div>
+                                <h4>Ingredients:</h4>
+                                <div>
+                                    <b>Malt:</b>
+                                    <ul>
+                                        {selectedBeer.ingredients.malt.map((malt, id) => (
+                                            <li key={id}>
+                                                {malt.name} - {malt.amount.value} {malt.amount.unit}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <div>
+                                    <b>Hops:</b>
+                                    <ul>
+                                        {selectedBeer.ingredients.hops.map((hop, id) => (
+                                            <li key={id}>
+                                                {hop.name} - {hop.amount.value} {hop.amount.unit} - {hop.add} -{' '}
+                                                {hop.attribute}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
+                ))
+            ) : (
+                <div className="spinner-container">
+                    <Spinner animation="border" role="status" className="spinner">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
                 </div>
-            ))}
+            )}
         </div>
     )
 }
--- src/components/BeerList/BeerList.tsx
+++ src/components/BeerList/BeerList.tsx
@@ -2,16 +2,27 @@ import { selectBeer } from '../../redux/beerSlice'
 import { useAppSelector } from '../../redux/hooks'
 import BeerCard from '../BeerCard/BeerCard'
 import './beerList.scss'
+import Spinner from 'react-bootstrap/Spinner'
 
 const BeerList = () => {
     const beer = useAppSelector(selectBeer)
 
     return (
-        <div className="container-sm beer-wrapper">
-            {beer.map((sBeer) => (
-                <BeerCard key={sBeer.id} {...sBeer} />
-            ))}
-        </div>
+        <>
+            {beer.length > 0 ? (
+                <div className="container-sm beer-wrapper">
+                    {beer.map((sBeer) => (
+                        <BeerCard key={sBeer.id} {...sBeer} />
+                    ))}
+                </div>
+            ) : (
+                <div className="spinner-container">
+                    <Spinner animation="border" role="status" className="spinner">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+            )}
+        </>
     )
 }
 
